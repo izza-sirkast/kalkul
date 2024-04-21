f10a8889511f1eab20782b9697e89522239619b4
izza-sirkast <dawlaizza405@gmail.com>
2024-04-21 20:58:48 +0700

    Working on the caret position of the calculator display

--- src/components/calculator/BasicCalculator.tsx
+++ src/components/calculator/BasicCalculator.tsx
@@ -1,53 +1,79 @@
-import React from 'react'
+"use client"
+
+import React, { ReactHTMLElement, useEffect, useState, useRef } from 'react'
 import Button1 from './calculatorButtons/Button1'
 
 type Props = {}
 
 export default function BasicCalculator({}: Props) {
+  const [display, setDisplay] = useState("")
+  const displayRef = useRef<HTMLInputElement>(null)
+
+  function inputHandler(input : React.ChangeEvent<HTMLInputElement>){
+    setDisplay(input.target.value)
+  }
+
+  function focusInput(){
+    displayRef.current?.focus()
+  }
+
+  // function changeCaretPos(inParenthesis : boolean){
+  //   displayRef.current?.focus();
+  //   if(inParenthesis){
+  //     setTimeout(() => {
+  //       if (displayRef.current !== null && displayRef.current.selectionStart !== null) {
+  //         const caretPos = displayRef.current.selectionStart - 1
+  //         displayRef.current.setSelectionRange(caretPos, caretPos);
+  //         }
+  //     }, 0);
+  //   }
+  // }
+  
+
   return (
     <div className='h-66 rounded-sm shadow-md bg-blue-200 px-3 py-3 calcuWidth1' >
         <section className='block w-full h-12 bg-blue-400 mb-3 flex justify-end items-center px-3'>
-            <p className='text-xl'>95</p>
+            <input className='text-xl text-right bg-blue-400 outline-none w-full' value={display} onChange={(e) => inputHandler(e)} ref={displayRef} />
         </section>
 
         <div className='flex w-full justify-between mb-2'>
-          <Button1 fontSize={"xl"}>Deg</Button1>
-          <Button1 fontSize={"xl"}>Rad</Button1>
-          <Button1>7</Button1>
-          <Button1>8</Button1>
-          <Button1>9</Button1>
-          <Button1 fontSize={"xl"}>DEL</Button1>
-          <Button1 fontSize={"xl"}>AC</Button1>
+          <Button1 fontSize={"xl"} displayRef={displayRef.current}>Deg</Button1>
+          <Button1 fontSize={"xl"} displayRef={displayRef.current}>Rad</Button1>
+          <Button1 setDisplay={setDisplay} focusInput={focusInput} displayRef={displayRef.current}>7</Button1>
+          <Button1 setDisplay={setDisplay} displayRef={displayRef.current}>8</Button1>
+          <Button1 setDisplay={setDisplay} displayRef={displayRef.current}>9</Button1>
+          <Button1 fontSize={"xl"} setDisplay={setDisplay} displayRef={displayRef.current}>DEL</Button1>
+          <Button1 fontSize={"xl"} setDisplay={setDisplay} displayRef={displayRef.current}>AC</Button1>
         </div>
 
         <div className='flex w-full justify-between mb-2'>
-          <Button1>&pi;</Button1>
-          <Button1 fontSize='xl'>sin</Button1>
-          <Button1>4</Button1>
-          <Button1>5</Button1>
-          <Button1>6</Button1>
-          <Button1 fontSize='3xl'>&#215;</Button1>
-          <Button1 fontSize='3xl'>&#247;</Button1>
+          <Button1 setDisplay={setDisplay} displayRef={displayRef.current}>&pi;</Button1>
+          <Button1 fontSize='xl' setDisplay={setDisplay} displayRef={displayRef.current}>sin</Button1>
+          <Button1 setDisplay={setDisplay} displayRef={displayRef.current}>4</Button1>
+          <Button1 setDisplay={setDisplay} displayRef={displayRef.current}>5</Button1>
+          <Button1 setDisplay={setDisplay} displayRef={displayRef.current}>6</Button1>
+          <Button1 fontSize='2xl' setDisplay={setDisplay} displayRef={displayRef.current}>&#215;</Button1>
+          <Button1 fontSize='2xl' setDisplay={setDisplay} displayRef={displayRef.current}>&#247;</Button1>
         </div>
 
         <div className='flex w-full justify-between mb-2'>
-          <Button1 fontSize='xl'>Log</Button1>
-          <Button1 fontSize='xl'>cos</Button1>
-          <Button1>1</Button1>
-          <Button1>2</Button1>
-          <Button1>3</Button1>
-          <Button1 fontSize={"3xl"}>+</Button1>
-          <Button1 fontSize={"3xl"}>-</Button1>
+          <Button1 fontSize='xl' setDisplay={setDisplay} displayRef={displayRef.current}>log</Button1>
+          <Button1 fontSize='xl' setDisplay={setDisplay} displayRef={displayRef.current}>cos</Button1>
+          <Button1 setDisplay={setDisplay} displayRef={displayRef.current}>1</Button1>
+          <Button1 setDisplay={setDisplay} displayRef={displayRef.current}>2</Button1>
+          <Button1 setDisplay={setDisplay} displayRef={displayRef.current}>3</Button1>
+          <Button1 fontSize={"2xl"} setDisplay={setDisplay} displayRef={displayRef.current}>+</Button1>
+          <Button1 fontSize={"2xl"} setDisplay={setDisplay} displayRef={displayRef.current}>-</Button1>
         </div>
 
         <div className='flex w-full justify-between'>
-          <Button1 fontSize={"xl"}>^</Button1>
-          <Button1 fontSize={"xl"}>tan</Button1>
-          <Button1>0</Button1>
-          <Button1>.</Button1>
-          <Button1>&#40;</Button1>
-          <Button1>&#41;</Button1>
-          <Button1 fontSize='3xl'>=</Button1>
+          <Button1 fontSize={"xl"} setDisplay={setDisplay} displayRef={displayRef.current}>^</Button1>
+          <Button1 fontSize={"xl"} setDisplay={setDisplay} displayRef={displayRef.current}>tan</Button1>
+          <Button1 setDisplay={setDisplay} displayRef={displayRef.current}>0</Button1>
+          <Button1 setDisplay={setDisplay} displayRef={displayRef.current}>.</Button1>
+          <Button1 setDisplay={setDisplay} displayRef={displayRef.current}>&#40;</Button1>
+          <Button1 setDisplay={setDisplay} displayRef={displayRef.current}>&#41;</Button1>
+          <Button1 fontSize='2xl' displayRef={displayRef.current}>=</Button1>
         </div>
     </div>
   )
--- src/components/calculator/calculatorButtons/Button1.tsx
+++ src/components/calculator/calculatorButtons/Button1.tsx
@@ -1,14 +1,66 @@
-import React from 'react'
+"use client"
+
+import React, { SetStateAction } from 'react'
 
 type Props = {
     children: React.ReactNode,
-    fontSize?: string
+    fontSize?: string,
+    setDisplay?: React.Dispatch<SetStateAction<string>>,
+    focusInput?: () => void,
+    displayRef?: any
 }
 
-export default function Button1({children, fontSize = "2xl"}: Props) {
+export default function Button1({children, fontSize = "2xl", setDisplay, focusInput, displayRef}: Props) {
+
+    function buttonHandler(){
+      const val = children?.toLocaleString()
+      const caretPos = displayRef.selectionStart
+      if(setDisplay){
+        switch(val){
+          case "AC":
+            setDisplay(lastDisplay => "")
+            displayRef.focus()
+            break
+          case "DEL":
+            const lastFirstSection = caretPos - 1 >= 0 ? caretPos - 1 : 0
+            setDisplay(lastDisplay => lastDisplay.slice(0, lastFirstSection) + lastDisplay.slice(caretPos, lastDisplay.length))
+            displayRef.focus()
+            setTimeout(() => {
+              displayRef.setSelectionRange(lastFirstSection, lastFirstSection)
+            }, 0)
+            break
+          case "sin":
+          case "cos":
+          case "tan":
+          case "log":
+            val && setDisplayAndCaret(val + '()', true)
+            break          
+          default:
+            val && setDisplayAndCaret(val, false)
+            break            
+        }
+      }
+    }
+
+    function setDisplayAndCaret(input:string, inParenthesis:boolean){
+      const caretPos = displayRef.selectionStart
+      if(setDisplay){
+        setDisplay(lastDisplay => {
+          return lastDisplay.slice(0,caretPos) + input + lastDisplay.slice(caretPos,lastDisplay.length) 
+        })
+      }
+      displayRef.focus()
+      setTimeout(() => {
+        inParenthesis 
+          ? displayRef.setSelectionRange(caretPos + 4, caretPos + 4)
+          : displayRef.setSelectionRange(caretPos + 1, caretPos + 1)
+      }, 0)
+
+    }
+
   return (
-    <div className={`bg-blue-300 w-12 h-10 flex justify-center items-center text-${fontSize} border border-blue-400 cursor-pointer hover:bg-blue-400`}>
+    <button onClick={buttonHandler} className={`bg-blue-300 w-12 h-10 flex justify-center items-center text-${fontSize} border border-blue-400 cursor-pointer hover:bg-blue-400`}>
         {children}
-    </div>
+    </button>
   )
 }
